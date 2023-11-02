d71d3b89734c5edb27aed0bf3b4d8a853842473a
El Amine Choubby <echoubby@c4r4p7.1337.ma>
2023-11-02 20:17:24 +0100

    lol

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,16 +6,37 @@
 /*   By: echoubby <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:10:09 by echoubby          #+#    #+#             */
-/*   Updated: 2023/11/02 17:17:15 by echoubby         ###   ########.fr       */
+/*   Updated: 2023/11/02 20:16:40 by echoubby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 void	*memmove(void *dest, const void *src, size_t n)
 {
 	size_t	i;
-	size_t len;
-	char	*dst;
-	const char	*src;
-	i = 0;
+	unsigned char *dst;
+	unsigned char *sors;
 
+	dst = (unsigned char *)dest;
+	sors = (unsigned char *)src;
+	i = n - 1;
+	if (dest > src)
+	{
+		while (i >= 0)
+		{
+			dst[i] = sors[i];
+			if (i == 0)
+				return (dest);
+			i--;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			dst[i] = sors[i];
+			i++;
+		}
+	}
+	return (dst);
 }
