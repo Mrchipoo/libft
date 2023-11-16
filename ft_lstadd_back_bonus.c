93921d39c2cfde3cc7ad1eaa67fedb160478ec2c
El Amine Choubby <echoubby@c4r1p10.1337.ma>
2023-11-16 08:23:12 +0100

    hh

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -15,6 +15,11 @@ void ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*last;
 
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
 	last = *lst;
 	while (last->next)
 		last = last->next;
